3de199088ed4459aa2cc691ba13524dc42856728
Piotr Wawrzyniak <piotr@wawrzyniak.poznan.pl>
2025-06-28 20:45:26 +0200

    Add more tests

--- tests/test_udg.py
+++ tests/test_udg.py
@@ -4,35 +4,38 @@ from udg import Graph, udg_recognition
 from Graph6Converter import Graph6Converter
 
 class TestUDG(unittest.TestCase):
-    def test_triangle(self):
-        g = Graph(3)
-        g.add_edge(0,1)
-        g.add_edge(1,2)
-        g.add_edge(0,2)
-        self.assertTrue(udg_recognition(g))
+    def test_udg_3(self):
+        self._check_udg(['3:1,2;2,3;3,1'], True)
 
-    def test_path_four(self):
-        g = Graph(4)
-        g.add_edge(0,1)
-        g.add_edge(1,2)
-        g.add_edge(2,3)
-        self.assertTrue(udg_recognition(g))
+    def test_udg_4(self):
+        self._check_udg([
+            '4:1,2;2,3;3,4',
+            '4:1,2;2,3;3,4;4,1',
+            '4:1,2;2,3;3,4;4,1;1,3',
+            '4:1,2;2,3;3,4;4,1;1,3;2,4',
+            ], True)
 
-    def test_non_udg(self):
-        non_udf = [
-            '5:1,2;1,3;1,4;2,5;3,5;4,5',
-            '6:1,2;1,3;2,4;3,4;2,5;3,6;5,6',
+    def test_non_udg_5(self):
+        self._check_udg(['5:1,2;1,3;1,4;2,5;3,5;4,5'], False)
+
+    def test_non_udg_6(self):
+        self._check_udg(['6:1,2;1,3;2,4;3,4;2,5;3,6;5,6'], False)
+        
+    def test_non_udg_7(self):
+        self._check_udg([
             '7:1,2;1,3;1,4;1,5;1,6;1,7',
             '7:1,2;1,3;2,4;3,4;2,5;3,7;5,6;6,7',
             '7:1,2;1,3;1,4;2,4;2,5;3,6;4,7;5,6;6,7',
             '7:1,2;1,3;1,4;2,5;2,7;3,6;4,7;5,6;6,7',
             '7:1,2;1,4;2,3;3,4;2,5;3,6;4,7;5,6;6,7'
-        ]
-
-        for graph in non_udf:
+            ], False)
+        
+    def _check_udg(self, graphs: list[str], expected: bool):
+        for graph in graphs:
+            print(f"Testing non-UDG graph: {graph}")
             nxg = Graph6Converter._parse_edge_list(graph)
             g = Graph(nxg)
-            self.assertFalse(udg_recognition(g), graph)
+            self.assertEqual(expected, udg_recognition(g), graph)
 
 if __name__ == '__main__':
     unittest.main()
